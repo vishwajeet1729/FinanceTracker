8edec90e5183aebbfaddd0e31b6fa87d2a1a4a61
vishwajeet1729 <vishwajeetwalse9767@gmail.com>
2025-07-03 13:56:11 +0530

    f

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -4,7 +4,7 @@ import App from './App.tsx'
 import './main.css'
 import { ClerkProvider } from '@clerk/clerk-react'
 
-const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
+const PUBLISHABLE_KEY ="pk_test_bmF0aXZlLXRhZHBvbGUtOTkuY2xlcmsuYWNjb3VudHMuZGV2JA"
 
 if (!PUBLISHABLE_KEY) {
   throw new Error("Missing Publishable Key")
@@ -13,7 +13,7 @@ if (!PUBLISHABLE_KEY) {
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
        
-    <ClerkProvider publishableKey="pk_test_bmF0aXZlLXRhZHBvbGUtOTkuY2xlcmsuY" afterSignOutUrl="/">
+    <ClerkProvider publishableKey="pk_test_bmF0aXZlLXRhZHBvbGUtOTkuY2xlcmsuYWNjb3VudHMuZGV2JA" afterSignOutUrl="/">
       <App />
     </ClerkProvider>
   </React.StrictMode>,
